88f55aaa226d44ec044ea5532b280f27d5b45dea
Pieter Van Der Helst <pieter.vanderhelst@hogent.be>
2021-09-01 17:51:29 +0200

    add nodemon

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const Koa = require('koa');
+const winston = require('winston');
+
 const app = new Koa();
 
-app.use(async (ctx, next) => {
-  console.log(ctx);
-  next();
-});
-app.use(async (ctx, next) => {
-  ctx.body = 'Hello world';
-  next();
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.simple(),
+  transports: [
+    new winston.transports.Console({silent: false})
+  ]
 });
 
 app.use(async (ctx, next) => {
-  console.log(ctx);
+  ctx.body = 'Goodbye world';
   next();
 });
+
+logger.info(`🚀 Server listening on http://localhost:9000`);
 app.listen(9000);
