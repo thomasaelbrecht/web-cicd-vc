72397f47d75febb256dd5c9f7153b9c6493215d1
Pieter Van Der Helst <pieter.vanderhelst@hogent.be>
2021-09-03 15:07:42 +0200

    add bodyparser

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const Koa = require('koa');
 const config = require('config');
-const { getLogger } = require('./core/logging')
+const { getLogger } = require('./core/logging');
+const bodyParser = require('koa-bodyparser');
 
 const NODE_ENV = config.get('env');
 const LOG_LEVEL = config.get('log.level');
@@ -12,8 +13,11 @@ const app = new Koa();
 const logger = getLogger();
 
 
+app.use(bodyParser());
+
 app.use(async (ctx, next) => {
   logger.info(JSON.stringify(ctx.request));
+  logger.info(JSON.stringify(ctx.request.body));
   if (ctx.request.method === 'GET' && ctx.request.url === "/api/transactions") {
     ctx.body = "[{'user': 'Benjamin', 'amount': 100, 'place': 'Irish Pub', date: '2021-08-15' }]";
   } else {
