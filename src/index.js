5ef25f4cdf7b80dd016e2e913814f7c1a031604e
Pieter Van Der Helst <pieter.vanderhelst@hogent.be>
2021-09-08 17:24:14 +0200

    add post route

--- src/index.js
+++ src/index.js
@@ -24,6 +24,11 @@ router.get('/api/transactions', async (ctx) => {
   ctx.body = transactionService.getAll();
 })
 
+router.post('/api/transactions', async (ctx) => {
+  const newTransaction = transactionService.create({...ctx.request.body, date: new Date(ctx.request.body.date)});
+  ctx.body = newTransaction;
+})
+
 app
   .use(router.routes())
   .use(router.allowedMethods());
--- src/service/transaction.js
+++ src/service/transaction.js
@@ -1,3 +1,5 @@
+const { getLogger } = require('../core/logging');
+
 let TRANSACTIONS = [{id: 1, user: 'Benjamin', amount: 100, place: 'Irish Pub', date: '2021-08-15' }];
 
 getAll = () => {
@@ -7,7 +9,10 @@ getAll = () => {
 getById = (id) => {  throw new Error("not implemented yet"); }
 
 create = ({amount, date, place, user}) => {
-  throw new Error("not implemented yet");
+  const maxId = Math.max(...TRANSACTIONS.map(i => i.id));
+  const newTransaction = {id: maxId+1, amount, date, place, user};
+  TRANSACTIONS = [...TRANSACTIONS, newTransaction];
+  return newTransaction;
 }
 
 updateById = (id, {amount, date, place, user}) => {
