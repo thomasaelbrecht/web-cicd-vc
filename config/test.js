84700447afbf639294ae32bbdd69985feef76c2c
Thomas Aelbrecht <thomasaelbrecht@live.com>
2021-11-23 09:49:35 +0100

    Update test config

--- config/test.js
+++ config/test.js
@@ -17,4 +17,18 @@ module.exports = {
 		limit: 100,
 		offset: 0,
   },
+	auth: {
+		argon: {
+			saltLength: 16,
+			hashLength: 32,
+			timeCost: 6,
+			memoryCost: 2 ** 17,
+		},
+		jwt: {
+			secret: 'eenveeltemoeilijksecretdatniemandooitzalradenandersisdesitegehacked',
+			expirationInterval: 60 * 60 * 1000, // ms (1 hour)
+			issuer: 'budget.hogent.be',
+			audience: 'budget.hogent.be',
+		},
+	}
 };
