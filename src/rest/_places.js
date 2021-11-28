02943d953db5045172ab9a9b4a0a62eeb8594995
Thomas Aelbrecht <thomasaelbrecht@live.com>
2021-11-28 10:14:24 +0100

    Add places validation

--- src/rest/_places.js
+++ src/rest/_places.js
@@ -1,9 +1,17 @@
+const Joi = require('joi');
 const Router = require('@koa/router');
 const placeService = require('../service/place');
 const { requireAuthentication } = require('../core/auth');
+const validate = require('./_validation.js');
 
 const getAllPlaces = async (ctx) => {
 	ctx.body = await placeService.getAll();
+};
+getAllPlaces.validationScheme = {
+  query: Joi.object({
+    limit: Joi.number().positive().max(1000).optional(),
+    offset: Joi.number().min(0).optional(),
+  }).and('limit', 'offset'),
 };
 
 const createPlace = async (ctx) => {
@@ -11,18 +19,43 @@ const createPlace = async (ctx) => {
 	ctx.body = newPlace;
 	ctx.status = 201;
 };
+createPlace.validationScheme = {
+  body: {
+    name: Joi.string().max(255),
+    rating: Joi.number().min(1).max(5).integer().optional(),
+  },
+};
 
 const getPlaceById = async (ctx) => {
 	ctx.body = await placeService.getById(ctx.params.id);
+};
+getPlaceById.validationScheme = {
+  params: {
+    id: Joi.string().uuid(),
+  },
 };
 
 const updatePlace = async (ctx) => {
 	ctx.body = await placeService.updateById(ctx.params.id, ctx.request.body);
 };
+updatePlace.validationScheme = {
+  params: {
+    id: Joi.string().uuid(),
+  },
+  body: {
+    name: Joi.string().max(255),
+    rating: Joi.number().min(1).max(5).integer(),
+  },
+};
 
 const deletePlace = async (ctx) => {
 	await placeService.deleteById(ctx.params.id);
 	ctx.status = 204;
+};
+deletePlace.validationScheme = {
+  params: {
+    id: Joi.string().uuid(),
+  },
 };
 
 /**
@@ -35,11 +68,11 @@ module.exports = (app) => {
 		prefix: '/places',
 	});
 
-	router.get('/', requireAuthentication, getAllPlaces);
-	router.post('/', requireAuthentication, createPlace);
-	router.get('/:id', requireAuthentication, getPlaceById);
-	router.put('/:id', requireAuthentication, updatePlace);
-	router.delete('/:id', requireAuthentication, deletePlace);
+	router.get('/', requireAuthentication, validate(getAllPlaces.validationScheme), getAllPlaces);
+	router.post('/', requireAuthentication, validate(createPlace.validationScheme), createPlace);
+	router.get('/:id', requireAuthentication, validate(getPlaceById.validationScheme), getPlaceById);
+	router.put('/:id', requireAuthentication, validate(updatePlace.validationScheme), updatePlace);
+	router.delete('/:id', requireAuthentication, validate(deletePlace.validationScheme), deletePlace);
 
 	app.use(router.routes()).use(router.allowedMethods());
 };
