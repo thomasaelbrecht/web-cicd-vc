04e65c1b48ba3df93c3ace4c0b7307a3c203ba14
Thomas Aelbrecht <thomasaelbrecht@live.com>
2021-11-28 10:12:35 +0100

    Add transaction validation

--- src/rest/_transactions.js
+++ src/rest/_transactions.js
@@ -1,12 +1,20 @@
+const Joi = require('joi');
 const Router = require('@koa/router');
 const transactionService = require('../service/transaction');
 const { requireAuthentication } = require('../core/auth');
+const validate = require('./_validation.js');
 
 const getAllTransactions = async (ctx) => {
 	const limit =  ctx.query.limit && Number(ctx.query.limit);
     const offset =  ctx.query.offset && Number(ctx.query.offset);
 	ctx.body = await transactionService.getAll(limit, offset);
 };
+getAllTransactions.validationScheme = {
+	query: Joi.object({
+		limit: Joi.number().positive().max(1000).optional(),
+		offset: Joi.number().min(0).optional(),
+	}).and('limit', 'offset'),
+ };
 
 const createTransaction = async (ctx) => {
 	const newTransaction = await transactionService.create({
@@ -17,9 +25,21 @@ const createTransaction = async (ctx) => {
 	ctx.body = newTransaction;
 	ctx.status=201;
 };
+createTransaction.validationScheme = {
+	body: {
+		amount: Joi.number().invalid(0),
+		date: Joi.date().iso().less('now'),
+		placeId: Joi.string().uuid(),
+  },
+};
 
 const getTransactionById = async (ctx) => {
 	ctx.body = await transactionService.getById(ctx.params.id);
+};
+getTransactionById.validationScheme = {
+  params: {
+    id: Joi.string().uuid(),
+  },
 };
 
 const updateTransaction = async (ctx) => {
@@ -29,10 +49,25 @@ const updateTransaction = async (ctx) => {
 		date: new Date(ctx.request.body.date),
 	});
 };
+updateTransaction.validationScheme = {
+  params: {
+    id: Joi.string().uuid(),
+  },
+  body: {
+    amount: Joi.number().invalid(0),
+    date: Joi.date().iso().less('now'),
+    placeId: Joi.string().uuid(),
+  },
+};
 
 const deleteTransaction = async (ctx) => {
 	await transactionService.deleteById(ctx.params.id);
 	ctx.status = 204;
+};
+deleteTransaction.validationScheme = {
+  params: {
+    id: Joi.string().uuid(),
+  },
 };
 
 /**
@@ -45,11 +80,11 @@ module.exports = (app) => {
 		prefix: '/transactions',
 	});
 
-	router.get('/', requireAuthentication, getAllTransactions);
-	router.post('/', requireAuthentication, createTransaction);
-	router.get('/:id', requireAuthentication, getTransactionById);
-	router.put('/:id', requireAuthentication, updateTransaction);
-	router.delete('/:id', requireAuthentication, deleteTransaction);
+	router.get('/', requireAuthentication, validate(getAllTransactions.validationScheme), getAllTransactions);
+	router.post('/', requireAuthentication, validate(createTransaction.validationScheme), createTransaction);
+	router.get('/:id', requireAuthentication, validate(getTransactionById.validationScheme), getTransactionById);
+	router.put('/:id', requireAuthentication, validate(updateTransaction.validationScheme), updateTransaction);
+	router.delete('/:id', requireAuthentication, validate(deleteTransaction.validationScheme), deleteTransaction);
 
 	app.use(router.routes()).use(router.allowedMethods());
 };
